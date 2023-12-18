eb88c7e28d487c077a9a75a4ad598114917fe5eb
roma <roma.okhrimenko@gmail.com>
2023-12-18 13:11:38 +0200

    Fix authorization background image for PC

--- src/modules/authorization/login/index.tsx
+++ src/modules/authorization/login/index.tsx
@@ -1,19 +1,18 @@
 'use client';
 import styles from './login.module.scss';
 import LoginForm from '@/modules/authorization/login/form/form';
-import AuthorizationMan from '@/assets/images/png/authorization/authorization-man.png';
-import AuthorizationWoman from '@/assets/images/png/authorization/authorization-woman.png';
 import Image from 'next/image';
+import AuthorizationBackground from '@/assets/images/png/authorization/authorization-background.png';
+
 const Login = () => {
-  return (
-    <div className={styles.wrapper}>
-      <Image className={styles.man_image} src={AuthorizationMan} alt="" />
-      <Image className={styles.woman_image} src={AuthorizationWoman} alt="" />
-      <div className={styles.form_container}>
-        <LoginForm />
-      </div>
-    </div>
-  );
+ return (
+  <div className={styles.wrapper}>
+   <Image className={styles.background} src={AuthorizationBackground} alt="" />
+   <div className={styles.form_container}>
+    <LoginForm />
+   </div>
+  </div>
+ );
 };
 
 export default Login;
--- src/modules/authorization/registration/index.tsx
+++ src/modules/authorization/registration/index.tsx
@@ -1,20 +1,19 @@
 'use client';
 import styles from './registration.module.scss';
 import RegistrationForm from '@/modules/authorization/registration/form/form';
-import AuthorizationMan from '@/assets/images/png/authorization/authorization-man.png';
-import AuthorizationWoman from '@/assets/images/png/authorization/authorization-woman.png';
+import AuthorizationBackground from '@/assets/images/png/authorization/authorization-background.png';
 import Image from 'next/image';
+
 const Registration = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.shadow} />
-      <Image className={styles.man_image} src={AuthorizationMan} alt="" />
-      <Image className={styles.woman_image} src={AuthorizationWoman} alt="" />
-      <div className={styles.form_container}>
-        <RegistrationForm />
-      </div>
-    </div>
-  );
+ return (
+  <div className={styles.wrapper}>
+   <div className={styles.shadow} />
+   <Image className={styles.background} src={AuthorizationBackground} alt="" />
+   <div className={styles.form_container}>
+    <RegistrationForm />
+   </div>
+  </div>
+ );
 };
 
 export default Registration;
